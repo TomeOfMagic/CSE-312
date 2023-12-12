6861b4548faeadc9aa54e091f562dc55cbbcf745
desktop-fbd3sph <jacobpham82@gmail.com>
2023-12-11 22:50:17 -0800

    change bash script for setup

--- setup.sh
+++ setup.sh
@@ -1,7 +1,16 @@
-curl https://github.com/facebookresearch/detectron2/archive/refs/tags/v0.4.zip
+#!/bin/bash -l
+
+wget https://github.com/facebookresearch/detectron2/archive/refs/tags/v0.4.zip
 
 unzip 'v0.4.zip'
 
 cd detectron2-0.4
 
 pip install -e .
+
+cd ..
+
+pip install -r requirements.txt
+
+#Remeber to download Git LFS For Model
+
