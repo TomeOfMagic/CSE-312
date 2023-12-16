b47974ffbb6f43ebc99290820895930eff56e20b
Henry Rodas <copy@Henrys-Air.attlocal.net>
2023-12-15 16:19:21 -0800

    edit setup.sh for model

--- setup.sh
+++ setup.sh
@@ -5,6 +5,11 @@ pip install torchvision
 
 pip install -r requirements.txt
 
+
+wget -v -O Model/output/model.zip -L https://ucmerced.app.box.com/shared/static/5cbui0nirr8t83gn1f6lvv8a7umn79tt
+
+unzip Model/output/model.zip -d Model/output
+
 wget https://github.com/facebookresearch/detectron2/archive/refs/tags/v0.4.zip
 
 unzip 'v0.4.zip'
@@ -14,7 +19,6 @@ cd detectron2-0.4
 pip install -e .
 
 cd ..
-
 
 
 #Remeber to download Git LFS For Model
